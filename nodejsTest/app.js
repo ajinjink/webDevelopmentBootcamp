4c1d3fb5980462ac05aa66ebf73de19e1719fa9d
Jina Kim <drjina02@gmail.com>
2023-06-30 01:11:34 +0900

    save data from server to file
    used file system and json files

--- nodejsTest/app.js
+++ nodejsTest/app.js
@@ -1,35 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
 
 app.get('/currenttime', function(req, res) {
     res.send('<h1>' + new Date().toISOString() + '</h1>');
 }); // localhost:3000/currenttime
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello World!</h1>');
+    res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"></form>');
 }); // localhost:3000/
 
+app.post('/store-user', function(req, res) {
+    const userName = req.body.username;
+
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData); // json -> raw js object
+
+    existingUsers.push(userName); // append
+
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers)); // js values -> raw json format text
+
+    res.send('<h1>Username stored!</h1>')
+});
+
 app.listen(3000);
-
-
-
-
-// function handleRequest(request, response) {
-    
-//     if (request.url === '/currenttime') {
-//         // localhost:3000/currenttime
-//         response.statusCode = 200;
-//         response.end('<h1>' + new Date().toISOString() + '</h1>');
-//     }
-//     else if (request.url === '/') {
-//         // localhost:3000
-//         response.statusCode = 200;
-//         response.end('<h1>Hello World!</h1>');
-//     }
-// }
-
-// const server = http.createServer(handleRequest);
-
-// server.listen(3000);
 
