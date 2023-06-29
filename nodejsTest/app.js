c57cf85746c216af7e1f31562617da7769689315
Jina Kim <drjina02@gmail.com>
2023-06-30 00:39:30 +0900

    used express package

--- nodejsTest/app.js
+++ nodejsTest/app.js
@@ -1,20 +1,35 @@
-const http = require('http');
+const express = require('express');
 
-function handleRequest(request, response) {
+const app = express();
+
+app.get('/currenttime', function(req, res) {
+    res.send('<h1>' + new Date().toISOString() + '</h1>');
+}); // localhost:3000/currenttime
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World!</h1>');
+}); // localhost:3000/
+
+app.listen(3000);
+
+
+
+
+// function handleRequest(request, response) {
     
-    if (request.url === '/currenttime') {
-        // localhost:3000/currenttime
-        response.statusCode = 200;
-        response.end('<h1>' + new Date().toISOString() + '</h1>');
-    }
-    else if (request.url === '/') {
-        // localhost:3000
-        response.statusCode = 200;
-        response.end('<h1>Hello World!</h1>');
-    }
-}
+//     if (request.url === '/currenttime') {
+//         // localhost:3000/currenttime
+//         response.statusCode = 200;
+//         response.end('<h1>' + new Date().toISOString() + '</h1>');
+//     }
+//     else if (request.url === '/') {
+//         // localhost:3000
+//         response.statusCode = 200;
+//         response.end('<h1>Hello World!</h1>');
+//     }
+// }
 
-const server = http.createServer(handleRequest);
+// const server = http.createServer(handleRequest);
 
-server.listen(3000);
+// server.listen(3000);
 
