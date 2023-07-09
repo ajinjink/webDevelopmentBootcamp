4eed725e2194d5777bc7b3cf8ed5194f8dc97318
Jina Kim <drjina02@gmail.com>
2023-07-09 21:50:40 +0900

    read file data & dynamic response

--- nodejsTest/app.js
+++ nodejsTest/app.js
@@ -12,7 +12,7 @@ app.get('/currenttime', function(req, res) {
 }); // localhost:3000/currenttime
 
 app.get('/', function(req, res) {
-    res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"></form>');
+    res.send('<form action="/store-user" method="POST"><label>Your Name: </label><input type="text" name="username"></form>');
 }); // localhost:3000/
 
 app.post('/store-user', function(req, res) {
@@ -29,6 +29,22 @@ app.post('/store-user', function(req, res) {
 
     res.send('<h1>Username stored!</h1>')
 });
+
+app.get('/users', function(req, res) {
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData); // json -> raw js object
+
+    let responseData = '<ul>';
+    for (const user of existingUsers) {
+        responseData += '<li>' + user + '</li>'
+    }
+
+    responseData += '</ul>';
+
+    res.send(responseData);
+})
 
 app.listen(3000);
 
