446cf6888e06c98fb09518b35078636cffc4857e
Jina Kim <drjina02@gmail.com>
2023-08-04 05:53:17 +0900

    can save posts to the db

--- sql-node/routes/blog.js
+++ sql-node/routes/blog.js
@@ -17,4 +17,15 @@ router.get('/new-post', async function(req, res) {
     res.render('create-post', {authors: authors});
 });
 
+router.post('/posts', async function(req, res) {
+    const data = [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.body.author
+    ];
+    await db.query('insert into posts (title, summary, body, author_id) values (?)', [data]);
+    res.redirect('/posts');
+});
+
 module.exports = router;
