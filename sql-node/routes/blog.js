80429e7be095b516df5fcbfb99a3ce9c3e861726
Jina Kim <drjina02@gmail.com>
2023-08-05 14:38:08 +0900

    display post, edit post, delete post

--- sql-node/routes/blog.js
+++ sql-node/routes/blog.js
@@ -10,8 +10,8 @@ router.get('/', function(req, res) {
 
 router.get('/posts', async function(req, res) {
     const query = `
-    select posts.*, authors.name as author_name 
-    from posts inner join authors on posts.author_id = authors.id
+        select posts.*, authors.name as author_name 
+        from posts inner join authors on posts.author_id = authors.id
     `;
     const [posts] = await db.query(query);
     res.render('posts-list', {posts: posts});
@@ -32,5 +32,63 @@ router.post('/posts', async function(req, res) {
     await db.query('insert into posts (title, summary, body, author_id) values (?)', [data]);
     res.redirect('/posts');
 });
+
+router.get('/posts/:id', async function(req, res) {
+    const query = `
+        select posts.*, authors.name as author_name, authors.email as author_email
+        from posts inner join authors on posts.author_id = authors.id
+        where posts.id = ?
+    `;
+
+    const [posts] = await db.query(query, [req.params.id]);
+
+    if (!posts || posts.length == 0) {
+        return res.status(404).render('404');
+    }
+
+    const postData = {
+        ...posts[0],
+        date: posts[0].date.toISOString(),
+        humanReadableDate: posts[0].date.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        })
+    };
+    res.render('post-detail', { post: postData });
+});
+
+router.get('/posts/:id/edit', async function( req, res) {
+    const query = `
+        select * from posts
+        where id = ?
+    `;
+
+    const [posts] = await db.query(query, [req.params.id]);
+
+    if (!posts || posts.length == 0) {
+        return res.status(404).render('404');
+    }
+
+    res.render('update-post', { post: posts[0] });
+});
+
+router.post('/posts/:id/edit', async function(req, res) {
+    const query = `
+        update posts
+        set title = ?, summary = ?, body = ?
+        where id = ?
+    `;
+
+    await db.query(query, [req.body.title, req.body.summary, req.body.content, req.params.id]);
+
+    res.redirect('/posts')
+});
+
+router.post('/posts/:id/delete', async function(req, res) {
+    await db.query('delete from posts where id = ?', [req.params.id]);
+    res.redirect('/posts');
+})
 
 module.exports = router;
