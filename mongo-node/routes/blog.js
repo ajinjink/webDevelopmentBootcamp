075a3eae36190ed12a3f6fa388ae253c3c027e0e
Jina Kim <drjina02@gmail.com>
2023-08-06 23:43:50 +0900

    insert post into db

--- mongo-node/routes/blog.js
+++ mongo-node/routes/blog.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const mongodb = require('mongodb');
+
+const db = require('../data/database');
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -11,7 +16,29 @@ router.get('/posts', function(req, res) {
 });
 
 router.get('/new-post', async function(req, res) {
-  res.render('create-post');
+  const authors = await db.getDb().collection('authors').find().toArray(); // return promise
+  res.render('create-post', {authors: authors});
+});
+
+router.post('/posts', async function(req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db.getDb().collection('authors').findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title, 
+    summary: req.body.summary, 
+    body: req.body.content, 
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email
+    }
+  };
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+  res.redirect('/posts');
 });
 
 module.exports = router;
