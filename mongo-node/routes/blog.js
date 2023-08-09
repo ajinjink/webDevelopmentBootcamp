c31898dbcb5b96ff18ba85ddf6126ed7a9b4216d
Jina Kim <drjina02@gmail.com>
2023-08-10 00:41:11 +0900

    errors have to be handled manually in async functions

--- mongo-node/routes/blog.js
+++ mongo-node/routes/blog.js
@@ -42,8 +42,16 @@ router.post('/posts', async function(req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts/:id', async function(req, res) {
+router.get('/posts/:id', async function(req, res, next) {
   const postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    // return res.status(404).render('404');
+    return next(error);
+  }
+
   const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)}, {summary: 0});
   if (!post) {
     return res.status(404).render('404');
