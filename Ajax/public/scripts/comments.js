bb3d5c12730e553d3448c9ee44f65a3bed720c06
Jina Kim <drjina02@gmail.com>
2023-11-19 01:14:48 +0900

    update dom from response data

--- Ajax/public/scripts/comments.js
+++ Ajax/public/scripts/comments.js
@@ -1,10 +1,31 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+const commentsSectionElement = document.getElementById('comments');
+
+function createCommentsList(comments) { // make comments arr into list
+    const commentListElement = document.createElement('ol');
+
+    for (const comment of comments) {
+        const commentElement = document.createElement('li');
+        commentElement.innerHTML = `
+            <article class="comment-item">
+            <h2>${comment.title}</h2>
+            <p>${comment.text}</p>
+            </article>
+            `;
+        commentListElement.append(commentElement);
+    }
+
+    return commentListElement;
+}
 
 async function fetchCommentsForPost(event) {
     const postId = loadCommentsBtnElement.dataset.postid;
     const response = await fetch(`/posts/${postId}/comments`); // url of get request
     const responseData = await response.json(); // browser method. parse json -> js object
-    console.log(responseData);
+    
+    const commentsListElement = createCommentsList(responseData);
+    commentsSectionElement.innerHTML = ''; // clear
+    commentsSectionElement.appendChild(commentsListElement);
 }
 
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
