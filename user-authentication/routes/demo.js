d9707ebb2fc41b9d51da94376fce10829fec106a
Jina Kim <drjina02@gmail.com>
2023-11-19 22:10:40 +0900

    sign in validation check

--- user-authentication/routes/demo.js
+++ user-authentication/routes/demo.js
@@ -23,6 +23,18 @@ router.post('/signup', async function (req, res) {
   const enteredConfirmEmail = userData['confirm-email']; // name에 - 있으면 속성 이름 .으로 접근 불가
   const enteredPassword = userData.password;
 
+  if (!enteredEmail || !enteredConfirmEmail || !enteredPassword || 
+    enteredPassword.trim() < 6 || enteredEmail !== enteredConfirmEmail || !enteredEmail.includes('@')) {
+      console.log("Incorrect data input");
+      return res.redirect('/signup');
+  }
+
+  const existingUser = await db.getDb().collection('users').findOne({email: enteredEmail});
+  if (existingUser) {
+    console.log('User exitsts already.');
+    return res.redirect('/signup');
+  }
+
   const hashedPassword = await bcrypt.hash(enteredPassword, 12); // 암호화 할 대상, 암호화 강도
 
   const user = {
@@ -35,7 +47,26 @@ router.post('/signup', async function (req, res) {
   res.redirect('/login');
 });
 
-router.post('/login', async function (req, res) {});
+router.post('/login', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db.getDb().collection('users').findOne({email: enteredEmail});
+  if (!existingUser) {
+    console.log('Login failed. Invalid email');
+    return res.redirect('/login');
+  }
+
+  const passwordEqual = await bcrypt.compare(enteredPassword, existingUser.password); // unhashed, hashed
+  if (!passwordEqual) {
+    console.log('Login failed. Wrong password');
+    return res.redirect('/login');
+  }
+
+  console.log('Login Succeeded');
+  res.redirect('/admin');
+});
 
 router.get('/admin', function (req, res) {
   res.render('admin');
